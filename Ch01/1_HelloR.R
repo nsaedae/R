e7b3ac09e2d65a01002844386ec9b8acfb999c70
unknown <nsaedae@gmail.com>
2022-04-18 15:43:39 +0900

    2022/04/18 Ch01 수업예제

---
+++ Ch01/1_HelloR.R
@@ -0,0 +1,15 @@
+#날짜 : 2022/04/18
+#이름 : 김철학
+#내용 : Hello R
+
+print("Hello R!")
+print('안녕 R!')
+
+hello <- "Hello World"
+print(hello)
+
+welcome <- 'Welcome R!'
+welcome
+
+greeting <- 'Greeting R!'
+greeting
